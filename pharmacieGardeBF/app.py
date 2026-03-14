7593d087f1f899ae5f25ab959220de4d8c8eacb4
BossoInoussa <bssnsstr@gmail.com>
2026-03-14 05:23:34 +0000

    Fix production server

--- pharmacieGardeBF/app.py
+++ pharmacieGardeBF/app.py
@@ -157,5 +157,8 @@ def api_garde():
     return jsonify({"groupe": garde_group, "timestamp": now.isoformat()})
 
 
+# if __name__ == '__main__':
+#    app.run(debug=True)
+
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 5000)), debug=False)
